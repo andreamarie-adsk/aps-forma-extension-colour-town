b2a81b7df73e38cf68931bbe4433a55898659f0b
Andrea Marie Festøy <andreamariefestoy@Andreas-MacBook-Pro.local>
2023-11-30 15:03:05 +0100

    feat: Add christmas tree

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,14 @@
+import AddChristmasTree from "./components/AddChristmasTree";
 import ColourBuildings from "./components/ColourBuildings";
+import DownloadChristmasTree from "./components/DownloadChristmasTree";
 
 export default function App() {
   return (
     <>
       <h1>Try a fun town</h1>
       <ColourBuildings />
+      <DownloadChristmasTree />
+      <AddChristmasTree />
     </>
   );
 }
